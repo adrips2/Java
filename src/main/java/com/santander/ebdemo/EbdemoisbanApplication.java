78090179a3eb21a09992697edcbeae0502b75a21
adrips2 <adrian.alvarez.gil@gmail.com>
2017-11-21 17:21:31 +0100

    Add files via upload

---
+++ src/main/java/com/santander/ebdemo/EbdemoisbanApplication.java
@@ -0,0 +1,12 @@
+package com.santander.ebdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbdemoisbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbdemoisbanApplication.class, args);
+	}
+}
